e6ab030600a76f91e14e117fc3a0cc23f7909287
AlexSiXiong <2428545784@qq.com>
2019-10-13 23:42:03 +1300

    naked twin completed

--- function.py
+++ function.py
@@ -5,6 +5,37 @@ from utils import *
 # (note: changing the value here will _not_ change the test code)
 # grid = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
 
+def nack3(values):
+    temp = values.copy()
+
+    naked_twins = {}
+    for unit in unitlist:
+        possible_naked_twins_in_one_unit = {}
+        for box in unit:
+            if len(values[box]) == 2:
+                possible_twin_value = values[box]
+                if possible_twin_value in possible_naked_twins_in_one_unit.keys():
+                    possible_naked_twins_in_one_unit[possible_twin_value].append(box)
+                else:
+                    possible_naked_twins_in_one_unit[possible_twin_value] = [box]
+
+        for key_one_twin_set, boxes in possible_naked_twins_in_one_unit.items():
+            if len(boxes) >= 2:
+                if key_one_twin_set in naked_twins.keys():
+                    naked_twins[key_one_twin_set].append(unit)
+                else:
+                    naked_twins[key_one_twin_set] = [unit]
+
+    for one_twin_set, units_list in naked_twins.items():
+        for unit_has_twins in units_list:
+            for box in unit_has_twins:
+                if temp[box] != one_twin_set:
+                    temp[box] = temp[box].replace(one_twin_set[0], '')
+                    temp[box] = temp[box].replace(one_twin_set[1], '')
+
+    return temp
+
+
 def grid_values(grid):
     dictionary = dict(zip(boxes, grid))
     for key, value in dictionary.items():
@@ -110,15 +141,19 @@ def search(values):
 if __name__ == '__main__':
     grid = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
     grid2 = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'
+    grid3 = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
+    dictionary2 = grid_values(grid3)
+    # dictionary2 = naked_twins2(dictionary2)
+    # dictionary2 = eliminate2(dictionary2)
+    # dictionary2 = only_choice2(dictionary2)
+    # dictionary2 = reduce_puzzle(dictionary2)
+    # dictionary2 = search(dictionary2)
 
-    dictionary2 = grid_values(grid2)
-    dictionary2 = eliminate2(dictionary2)
-    dictionary2 = only_choice2(dictionary2)
-    dictionary2 = reduce_puzzle(dictionary2)
-    dictionary2 = search(dictionary2)
-    display(dictionary2)
-    print(dictionary2)
-
+    print('xxxxxxxx')
+    # print('xxxxxxxx')
+    # display(after)
+    #
+    #
     # temp_dic = {}
     # temp_dic['a'] = 3
     # temp_dic['b'] = 3
@@ -126,6 +161,23 @@ if __name__ == '__main__':
     # key, value = min((key, temp_dic[key]) for key in temp_dic)
     # print(key)
     # print(value)
-
-    print(dictionary2.keys())
+    # for i in temp_dic:
+    #     print(i)
+    #
+    # list1 = ['z1','a', 'b','c']
+    # list2 = ['z2','c','d','e']
+    # print(list1)
+    # list1 = set(list1)
+    # list1.remove('z1')
+    # print(list1)
+    # # list2 = set(list2)
+    # # list3 = list1 | list2
+    # # print(list3)
+    # print(unitlist)
+    #
+    #
+    reversedcol = cols[::-1]
+    diagdown = [[rows[i] + cols[i] for i in range(len(rows))]]
+    print(diagdown)
+    diagup = [[rows[i] + reversedcol[i] for i in range(len(rows))]]
 
